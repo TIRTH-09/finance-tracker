6e67c731485c7a6a467e04e44d1c7974e14463e1
Tirth Bhesdadiya <tirthbhesdadiya4444@gmail.com>
2025-12-30 19:21:59 +0530

    Phase 3: Basic MVC flow working

---
+++ app/view/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Finance Tracker</title>
+</head>
+<body>
+    <h1>Finance Tracker MVC is Working 🎉</h1>
+    <p>Welcome to your first MVC page.</p>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/../app/controller/HomeController.php';
+
+$controller = new HomeController();
+$controller->index();
